9cd472381b3867b1973c50f4355652917eda9e72
Le Toan <letoan285@gmail.com>
2017-08-12 20:37:23 +0700

    Add more code

---
+++ app.js
@@ -0,0 +1,28 @@
+//var a = 5;
+// var numA = 50;
+// var b = 'name';
+// console.log(b);
+// var $c = 5;
+// var X = 'ban da sai';
+// console.log($c);
+// console.log(X);
+// var m = '';
+// console.log(m);
+//console.log(z);
+
+// var arr = {age: 31, name: 'Toanlv'};
+// console.log(arr.age + ' ' + arr.name);
+// var numB = 10;
+
+// console.log(numB);
+// var age = 22;
+// age == 32 ? 'Toan' : 'Tri';
+var numC = 80;
+if(numC > 20 && numC <=60 ){
+	console.log('You are old enough!');
+} else if (numC > 60) {
+
+	console.log('You are too old');
+} else {
+	console.log('you are too young');
+}
